06e29c85afbe747ada3dae433830981ffea76199
seperman <seperman.jh@gmail.com>
2022-08-23 00:36:35 +0900

    #Basic Request & Response Scenario 1) Middleware (res,req, next) 2) app.use 사용

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 import express from "express";
 const app = express();
 
-const Home = (req, res) => {
-  return res.send(req.headers);
-};
+const Home = (req, res) => res.send(req.route);
+const Middle = (req, res, next) => console.log(next());
 
+app.use(Middle);
 app.get("/", Home);
-app.get("/edit", Home);
 
 app.listen(4000);
