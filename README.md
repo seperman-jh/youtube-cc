173a5be70f43ce3eb2a2c60ac69e0e3f7066959a
seperman <seperman.jh@gmail.com>
2022-08-24 01:18:37 +0900

    #Basic Request & Response Scenario 1) README.md 추가 2) Logger 사용 3) Route 사용 (app.use)

---
+++ README.md
@@ -0,0 +1,5 @@
+/ : HOME
+
+/user/login : USER LOGIN
+
+/videos/:id : VIDEO WATCH
--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import express from "express";
+import morgan from "morgan"
+import userRouter from "./routes/userRouter";
+
+
 const app = express();
-
+const Logger = morgan("");
 const Home = (req, res) => res.send(req.route);
-const Middle = (req, res, next) => console.log(next());
 
-app.use(Middle);
-app.get("/", Home);
+
+app.use("/user",Logger, userRouter);
+app.get("/", Logger, Home );
 
 app.listen(4000);
